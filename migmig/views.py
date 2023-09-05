e961544b22c88ce75272db9bfd1c4548408081f2
strasse34 <rmirzaie2021@gmail.com>
2023-09-05 09:09:57 +0000

    1- Fixed: bug that not getting username in all pages with defining new base class to be inherited by other classes. 2- Add: delete, edite view, template and url of flight cards. 3- Bug: there is problem with slug. I cant get pages.

--- migmig/views.py
+++ migmig/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render, redirect, get_object_or_404, reverse
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views import generic, View
 from .forms import AddFlightForm
 from .models import FlightDetails
@@ -10,16 +10,10 @@ from django.contrib import messages
 from django.utils import timezone
 
 
-class HomeView(generic.ListView):
-    model = FlightDetails  
-    template_name = 'index.html'  
-    queryset = FlightDetails.objects.filter(status=1).order_by("-created_on")
-    paginate_by = 6
-
+class ContextMixin:
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['username'] = self.request.user.username  
-        self.status_convertor()
         return context
 
     def status_convertor(self):
@@ -28,7 +22,19 @@ class HomeView(generic.ListView):
         outdated_flight.update(status=0)
 
 
-class AddFlightView(LoginRequiredMixin, CreateView):
+class HomeView(ContextMixin, generic.ListView):
+    model = FlightDetails  
+    template_name = 'index.html'  
+    queryset = FlightDetails.objects.filter(status=1).order_by("-created_on")
+    paginate_by = 6
+
+    def status_convertor(self):
+        current_date = timezone.now().date()
+        outdated_flight = FlightDetails.objects.filter(flight_date__lt=current_date, status=1)
+        outdated_flight.update(status=0)
+
+
+class AddFlightView(LoginRequiredMixin, ContextMixin, CreateView):
     model = FlightDetails
     template_name = 'add_flight.html'  
     form_class = AddFlightForm
@@ -43,10 +49,9 @@ class AddFlightView(LoginRequiredMixin, CreateView):
         else:
             messages.error(self.request, "Flight date must be in the future.")
             return self.form_invalid(form)
-    
         
 
-class MyFlightsView(generic.ListView):
+class MyFlightsView(LoginRequiredMixin, ContextMixin, generic.ListView):
     model = FlightDetails  
     template_name = 'my_flight.html'  
     queryset = FlightDetails.objects.all().order_by("-created_on")
@@ -56,14 +61,38 @@ class MyFlightsView(generic.ListView):
         queryset = FlightDetails.objects.filter(
             traveler__id=self.request.user.id).order_by('-created_on')
         return queryset
+    
 
 
-class TravelerContactView(View):
-   
+class TravelerContactView(LoginRequiredMixin, ContextMixin, View):
     def get(self, request, slug, *arg, **kwargs):
         queryset = FlightDetails.objects.filter(status=1)
         flightinfo = get_object_or_404(queryset, slug=slug)
 
         return render(request, "traveler_contact.html", {"flightinfo": flightinfo})
 
+
+class EditFlightView(LoginRequiredMixin, ContextMixin, UpdateView):
     
+    model = FlightDetails
+    form_class = AddFlightForm
+    template_name = 'edit_flight.html'
+    success_url = reverse_lazy('my_flights')
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        msg = "Your flight details was updated successfully"
+        messages.add_message(self.request, messages.SUCCESS, msg)
+        return super(EditFlightsView, self).form_valid(form)
+
+
+class DeleteFlightView(LoginRequiredMixin, ContextMixin, DeleteView):
+    
+    model = FlightDetails
+    template_name = 'delete_fight.html'
+    success_url = reverse_lazy('my_flights')
+
+    def delete(self, request, *args, **kwargs):
+        msg = "Your flight has been deleted"
+        messages.add_message(self.request, messages.SUCCESS, msg)
+        return super(DeleteFlightsView, self).delete(request, *args, **kwargs)
