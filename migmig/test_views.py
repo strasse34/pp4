141398799406e76d3a781865e5c12b9333a15ac2
strasse34 <rmirzaie2021@gmail.com>
2023-09-15 13:21:54 +0000

    Add: test for views

--- migmig/test_views.py
+++ migmig/test_views.py
@@ -26,31 +26,45 @@ class FlightDetailsViewTestCase(TestCase):
             email='test@example.com'
         )
 
+    def setUp(self):
+        self.user = User.objects.create_user(username='testuser', password='testpassword')
+        self.client.login(username='testuser', password='testpassword')
+        self.flight = FlightDetails.objects.create(
+        traveler=self.user,
+        origin='Origin Value',  
+        destination='Destination Value',  
+        flight_date='2023-09-10', 
+        fname='First Name',  
+        lname='Last Name',  
+        weight_capacity=42.0,
+        address= 'Berlin Street 23',  
+    )
+        self.client.login(username='testuser', password='testpassword')
+
     def test_home_view(self):
         response = self.client.get(reverse('home'))
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'index.html')
-        self.assertContains(response, 'Upcoming Flights')
-        # You can add more assertions based on your view's content
+        
 
     def test_add_flight_view(self):
         self.client.login(username='testuser', password='testpassword')
         response = self.client.get(reverse('add_flight'))
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'add_flight.html')
-        # You can add more assertions based on your view's content
+        
 
     def test_my_flights_view(self):
         self.client.login(username='testuser', password='testpassword')
         response = self.client.get(reverse('my_flights'))
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'my_flight.html')
-        # You can add more assertions based on your view's content
+        
 
     def test_traveler_contact_view(self):
         response = self.client.get(reverse('traveler_contact', args=[self.flight.slug]))
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'traveler_contact.html')
-        # You can add more assertions based on your view's content
+        
 
     
